d3a1620b67744961e09c9b1b6aee573eb8e5c084
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-23 15:14:05 +0100

    the code prints the numbers between 1 and 100

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// - IF the number is a multiple of 3, the code prints "Fizz" instead;
+// - ELSE IF the number is a multiple of 5, the code prints "Buzz";
+// - ELSE IF the number is a multiple of 3 AND a multiple of 5, the code prints "FizzBuzz";
+
+
+// - Creation of a HTML container;
+// - Using the `append()` function, the code adds a HTML element to the container;
+// - This element contains the correct number or string;
+// - Using the `for` statement, the code adds all the numbers between 1 and 100 to the container;
+
+
+// - Using the same `for` statement, the code applies different styles to the elements;
+// - IF the element is a "Fizz" string, its background-color is green;
+// - ELSE IF the element is a "Fizz" string, its background-color is green;
+// - ELSE IF the element is a "FizzBuzz" string, its background-color is red;
+// - ELSE the background-color is blue.
+
+
+// Using the `for` statement, the code creates a loop and prints in console the numbers between 1 and 100
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+};
