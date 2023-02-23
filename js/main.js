061e06fd4e14a648bf8d399eba6e493b199db175
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-23 17:59:55 +0100

    begin different styles box

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-// - Using the same `for` statement, the code applies different styles to the elements;
-// - IF the element is a "Fizz" string, its background-color is green;
-// - ELSE IF the element is a "Fizz" string, its background-color is green;
-// - ELSE IF the element is a "FizzBuzz" string, its background-color is red;
-// - ELSE the background-color is blue.
-
-
 // Using the `for` statement, the code creates a loop and prints in console the numbers between 1 and 100
 for (let i = 1; i <= 100; i++) {
   // Using the `append()` function, the code adds a HTML element to the container
@@ -12,20 +5,28 @@ for (let i = 1; i <= 100; i++) {
   const container = document.querySelector('.container');
   const box = document.createElement('div');
 
-  // IF the number is a multiple of 3 AND a multiple of 5, the code prints "FizzBuzz" instead
-  // ELSE IF the number is a multiple of 5, the code prints "Buzz"
-  // ELSE IF the number is a multiple of 3, the code prints "Fizz"
+  // Using the same `for` statement, the code applies different styles to the elements
+  box.classList.add("box");
+
+  // IF the number is a multiple of 3 AND a multiple of 5, the code prints "FizzBuzz" instead and its background-color is red
+  // ELSE IF the number is a multiple of 3, the code prints "Fizz" and its background-color is green
+  // ELSE IF the number is a multiple of 5, the code prints "Buzz" and its background-color is yellow
+  // ELSE the code prints the proper number and its background-color is blue
   if (i % 3 == 0 && i % 5 == 0) {
     console.log('FizzBuzz');
+    box.classList.add("fizzbuzz");
     box.append('FizzBuzz');
   } else if (i % 3 == 0) {
     console.log('Fizz');
+    box.classList.add("fizz");
     box.append('Fizz');
   } else if (i % 5 == 0) {
     console.log('Buzz');
+    box.classList.add("buzz");
     box.append('Buzz');
   } else {
     console.log(i);
+    box.classList.add("index");
     box.append(i);
   }
 
